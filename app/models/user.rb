2f03700e8a8a94740e78bc7c45c64e492f656a9c
Anish Aggarwal <anish17122000@gmail.com>
2020-11-09 18:20:45 +0100

    made cols not-null

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+
+    before_create :hash
+
+    def hash
+        # self.password = hash password
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -16,9 +16,9 @@ ActiveRecord::Schema.define(version: 2021_03_27_134017) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "password"
-    t.string "email"
+    t.string "name", null: false
+    t.string "password", null: false
+    t.string "email", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
