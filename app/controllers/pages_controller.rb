f64512201207879eb453953351adb0fe603499fa
Anish Aggarwal <anish17122000@gmail.com>
2020-11-09 19:26:23 +0100

    added auth-checker

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,21 @@
 class PagesController < ActionController::Base
     def index
+        # authorize_request
     end
+
+
+    private
+    def authorize_request
+        header = request.headers['x-auth-token']
+        header = header.split(' ').last if header
+        begin
+          @decoded = JsonWebToken.decode(header)
+          current_user_id = @decoded["id"]
+        #   render json: { }, status: 422
+        rescue ActiveRecord::RecordNotFound => e
+          render json: { errors: e.message }, status: :unauthorized
+        rescue JWT::DecodeError => e
+          render json: { errors: e.message }, status: :unauthorized
+        end
+    end    
 end
