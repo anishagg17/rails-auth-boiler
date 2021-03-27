699d6a0ff9e7b55f69a2d065918bb26cd370acf5
Anish Aggarwal <anish17122000@gmail.com>
2021-03-27 19:21:46 +0530

    made cols not-null

--- db/migrate/20210327134017_create_users.rb
+++ db/migrate/20210327134017_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :password
-      t.string :email
+      t.string :name, null: false
+      t.string :password, null: false
+      t.string :email, null: false
 
       t.timestamps
     end
